a256fcd60e40d1a613a5265159b699cde479ec04
Mahmud-cse <52972839+Mahmud-cse@users.noreply.github.com>
2021-02-01 02:37:36 +0600

    Ninth commit

---
+++ Messaging_App -2/src/sample/Controller.java
@@ -0,0 +1,75 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Controller {
+    @FXML
+    TextField msgTextField;
+    @FXML
+    Button sendButton;
+
+    @FXML
+    TextArea allTextArea;
+
+    private BufferedReader reader;
+    private  BufferedWriter writer;
+
+    public Controller(BufferedReader reader, BufferedWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
+
+    public Controller(){
+
+        try {
+            Socket socket = new Socket("127.0.0.1", 5000);
+            OutputStreamWriter o=new OutputStreamWriter(socket.getOutputStream());
+            writer=new BufferedWriter(o);
+
+            InputStream in;
+            InputStreamReader isr=new InputStreamReader(socket.getInputStream());
+            reader=new BufferedReader(isr);
+
+            //writer.write(Name+"\n");
+            //writer.flush();
+            Thread t=new Thread(){
+                public void run(){
+                    try{
+                        String line=reader.readLine()+"\n";
+                        while(line!=null){
+                            allTextArea.appendText(line);
+                            line=reader.readLine()+"\n";
+                        }
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            };
+            t.start();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void msgSend(){
+        String Name="Asif:";
+        try {
+            String msg = msgTextField.getText() + "\n";
+            msgTextField.setText("");
+            writer.write(Name+msg+"\n");
+            BufferedWriter bw =new BufferedWriter(new FileWriter("Files/file.txt"));
+            bw.write("Asif:"+msg+"\n");
+            writer.flush();
+            bw.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
